f297df1f21de994de893b15ff306b00124aeeb31
Darllan <darllangomes10@hotmail.com>
2019-11-13 17:33:50 -0300

    modificação na classe perfil 2

--- src/sample/Perfil.java
+++ src/sample/Perfil.java
@@ -1,10 +1,54 @@
 package sample;
 
+import java.util.Objects;
+
 public class Perfil {
     private String nome;
     private int Id;
     private String senha;
-    private boolean genero1;
-    private boolean genero2;
-    private boolean genero3;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Perfil perfil = (Perfil) o;
+        return Id == perfil.Id &&
+                nome.equals(perfil.nome) &&
+                senha.equals(perfil.senha);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, Id, senha);
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public Perfil(String nome, int id, String senha) {
+        this.nome = nome;
+        Id = id;
+        this.senha = senha;
+    }
 }
