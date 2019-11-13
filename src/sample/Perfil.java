b7427d87af8ef58da7aa4be97256969ce40847dd
Darllan <darllangomes10@hotmail.com>
2019-11-13 17:29:57 -0300

    modificação na classe perfil

--- src/sample/Perfil.java
+++ src/sample/Perfil.java
@@ -1,4 +1,10 @@
 package sample;
 
 public class Perfil {
+    private String nome;
+    private int Id;
+    private String senha;
+    private boolean genero1;
+    private boolean genero2;
+    private boolean genero3;
 }
